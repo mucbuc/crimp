720e5970498c99c652d9097ff5d468c4a06f1ea1
markymark <smallitle@hotmail.com>
2014-10-30 06:55:30 -0700

    update.js

--- bin/update.js
+++ bin/update.js
@@ -4,11 +4,20 @@ var assert = require( 'assert' )
   , cp = require( 'child_process' )
   , Reader = require( './Reader' );
 
-Reader.readDependencies().forEach( function( dependency ) {
-	var name = Reader.libName( dependency );
-	cp.exec( 
-		'git subtree pull -P ' + name + ' ' + name + ' master --squash', 
-		function(error, stdout, stderr) {
-			console.log( stdout );
-		} ); 
-} ); 
+function update( dependencies, index ) {
+	if (typeof index === 'undefined') {
+		index = 0;
+	}
+
+	if (index < dependencies.length) {
+		var name = Reader.libName( dependencies[index] );
+		cp.exec( 
+			'git subtree pull -P ' + name + ' ' + name + ' master --squash', 
+			function(error, stdout, stderr) {
+				console.log( stdout );
+				update( dependencies, index + 1 );
+			} ); 
+	}
+}
+
+update( Reader.readDependencies() ); 
