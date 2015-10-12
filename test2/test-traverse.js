7aa6f456c36a1cb836a3276304613f25ce413d16
mbusenitz <mbusenitz@gmail.com>
2015-10-12 09:00:43 -0700

    separarted tests

--- test2/test-traverse.js
+++ test2/test-traverse.js
@@ -7,13 +7,12 @@ var ggf = require( '../bin/ggf.js' )
 test( 'ggf recursion', function(t) {
 	var expected = { 
 		"sources": [
-			'src/main.cpp', 
 			'lib/sublib/src/subsrc.h', 
 			'lib/sublib/src/subsrc.cpp', 
 			'lib/sublib2/src/sub2src.cpp'
 		]
 	};		
-	ggf( './test.json', function(gyp) {
+	ggf( './test-import.json', function(gyp) {
 		t.deepEqual( gyp, expected ); 
 		t.end();
 	} );
