0d16fc123c8cdea0d41442977ed675228426f2c4
mbusenitz <mbusenitz@gmail.com>
2015-10-13 22:28:50 -0700

    fix tests

--- test/test.js
+++ test/test.js
@@ -4,77 +4,71 @@ var assert = require( 'assert' )
   , cp = require( 'child_process' )
   , path = require( 'path' )
   , Expector = require( 'expector' ).Expector
-  , test = require( 'tape' );
+  , test = require( 'tape' )
+  , thisPath = path.dirname(__filename)
+  , plankScript = path.join( thisPath, '../bin/test.js' );
+
+process.chdir( thisPath ); 
 
 test( 'test build', function(t) {
-	var controller = new Expector();
-
-	t.plan( 1 ); 	
-
+	var controller = makeExpector(t);
 	controller.expect( 'built' ); 
 	controller.expect( 'hello test\n' );
 
 	cp
-	.fork( getPlankTestScript() )
+	.fork( plankScript )
 	.on( 'exit', function() {
-		var p = path.join( __dirname, '/build/Test/test' );
-		console.log( p );
 		controller.emit( 'built' );
-		cp.execFile( p, function(err, stdout, stderr) {
-			if(err) throw err;
-			controller.emit( stdout );
-			controller.check();
-			t.pass();
-		} ); 
+		runBuild( './build/Test/test', controller );  
 	});
 });
 
 test( 'release build', function(t) {
-	var controller = new Expector();
-
-	t.plan( 1 ); 	
+	var controller = makeExpector(t);
 	controller.expect( 'built' ); 
 	controller.expect( 'hello release\n' );
 
 	cp
 	.fork( 
-		  getPlankTestScript()
+		  plankScript
 		, ['-r'] )
 	.on( 'exit', function() {
 		controller.emit( 'built' );
-		cp.execFile( path.join( __dirname, '/build/Release/test' ), function(err, stdout, stderr) {
-			if(err) throw err;
-			controller.emit( stdout );
-			controller.check();
-			t.pass();
-		} ); 
+		runBuild( './build/Release/test', controller ); 
 	});
-
 });
 
 test( 'debug build', function(t) {
-	var controller = new Expector();
-
-	t.plan( 1 ); 	
+	var controller = makeExpector(t);
 	controller.expect( 'built' ); 
 	controller.expect( 'hello debug\n' );
 
 	cp
 	.fork( 
-		  getPlankTestScript()
+		  plankScript
 		, ['-d'] )
 	.on( 'exit', function() {
 		controller.emit( 'built' );
-		cp.execFile( path.join( __dirname, '/build/Debug/test' ), function(err, stdout, stderr) {
-			if(err) throw err;
-			controller.emit( stdout );
-			controller.check();
-			t.pass();
-		} ); 
+ 	 	runBuild( './build/Debug/test', controller );
 	});
-
 });
 
-function getPlankTestScript() {
-	return path.join( __dirname, '../bin/test.js' );
+function makeExpector(tape) {
+	var controller = new Expector(tape)
+	  , tmpCheck = controller.check; 
+
+	controller.check = function() {
+		tmpCheck();
+		tape.end();
+	};
+
+	return controller;
+}
+
+function runBuild( path, controller ) {
+	cp.execFile( path, function(err, stdout, stderr) {
+		if(err) throw err;
+		controller.emit( stdout );
+		controller.check();
+	} );
 }
