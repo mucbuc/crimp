8047b2439c04531f91e7028af5534913e949eb18
mbusenitz <mbusenitz@gmail.com>
2015-12-23 21:44:12 -0800

    fix conflict

--- test/src/main-data.cpp
+++ test/src/main-data.cpp
@@ -6,22 +6,6 @@ int main(int argc, const char * argv[])
 {
 	using namespace std;
 	
-<<<<<<< HEAD
 	cout << json<>()._hello << endl;
-=======
-
-#ifdef TARGET_TEST
-	string var( "hello test" );
-#elif 1
-#ifndef NDEBUG
-	string var( "hello debug" ); 
-#else
-	string var( "hello release" );
-#endif
-#endif	 
-//	ASSERT( var.size() )(var);
-
-	cout << var << endl;
->>>>>>> 0ae3a95f95631a2eb135ebddb06991b9138315fb
 	return 0; 
 }
