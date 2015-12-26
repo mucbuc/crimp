f18d03a54f3002520da1f4cf258e101565c4dba3
mbusenitz <mbusenitz@gmail.com>
2015-12-26 04:56:07 -0800

    prevent file access

--- test/test.js
+++ test/test.js
@@ -56,7 +56,24 @@ test( 'define recursion', function(t) {
       '../lib/sublib/src/subsrc.cpp', 
       '../lib/sublib2/src/subsrc.cpp'
     ];    
-  define( './test-import.json' ).then( function(gyp) {
+  define( './test-import.json', function(path, cb) {
+
+    console.log( '******* path: ' path ); 
+    process.exit(0); 
+    if (path == path) 
+    {
+      cb( {
+        "import": [
+          "lib/sublib2/def.json"
+        ],
+        "sources": [
+          "src/subsrc.h",
+          "src/subsrc.cpp"
+        ]
+      } );
+    }
+
+  } ).then( function(gyp) {
     t.assert( gyp.hasOwnProperty( 'sources' ) );
     t.deepEqual( gyp.sources, expected ); 
     t.end();
