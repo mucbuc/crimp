3dbec23d205868c3aa6efafce82536b46641b8d8
mucbuc <mbusenitz@gmail.com>
2016-01-17 01:55:39 -0800

    runner use context spawn

--- bin/runner.js
+++ bin/runner.js
@@ -1,35 +1,32 @@
-var cp = require( 'child_process' )
-  , Promise = require( 'promise' )
+var Promise = require( 'promise' )
   , join = require( 'path' ).join;
 
 function run(context) {
   return new Promise( function( resolve, reject) {
 
-    var cwd = join(context.testDir, context.tempDir )
-      , execPath = context.gcc ? 'out' : 'build';
+    var execPath = context.gcc ? 'out' : 'build';
+    if (context.release) {
+      execPath = join( execPath, 'Release');
+    }
+    else if (context.test) {
+      execPath = join( execPath, 'Test');
+    }
+    else {
+      execPath = join( execPath, 'Debug');
+    }
 
-      if (context.release) {
-        execPath = join( execPath, 'Release');
-      }
-      else if (context.test) {
-        execPath = join( execPath, 'Test');
-      }
-      else {
-        execPath = join( execPath, 'Debug');
-      }
+    execPath = join( execPath, context.targetName );
 
-      execPath = join( execPath, context.targetName );
-
-      cp.spawn( 
-        execPath,
-        [], 
-        { stdio: 'inherit', cwd: cwd } )
-      .on( 'exit', function(code) {
-        if (code)
-          reject();
-        else
-          resolve();
-      });
+    context.spawn( 
+      execPath,
+      [], 
+      context.tempDir )
+    .on( 'exit', function(code) {
+      if (code)
+        reject();
+      else
+        resolve();
+    });
   });
 
 }
