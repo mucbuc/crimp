e176f29813422e0929183b104147097eab7d73c2
mbusenitz <mbusenitz@gmail.com>
2016-01-27 19:51:38 -0800

    asserter rid

--- def/plank.gypi
+++ def/plank.gypi
@@ -21,18 +21,6 @@
 	       			'TARGET_TEST=1'
 	       		],
 	       	}	
-	    },
-		"sources": 
-			[
-				"../src/archiver.cpp",
-				"../src/archiver.h",
-				"../src/asserter.cpp",
-				"../src/asserter.h",
-				"../src/asserter.hxx",
-				"../src/test.h"
-			],
-        'include_dirs': [
-			'../src/',
-		],	
-    }
+	    }
+	}
 }
--- test/test.js
+++ test/test.js
@@ -11,31 +11,6 @@ var assert = require( 'assert' )
 
 process.chdir( thisPath );
 
-test( 'asserter', function(t) {
-  var controller = new Expector(t)
-   ,  resultPath = './tmp/result.json';
-
-  controller
-  .expect( 'not exits' )
-  .expect( 'exits' )
-  .expect( 0 );
-
-  fs.unlink( resultPath, function(err) {
-    tryOpen();
-    crimp([ '-p', 'check_assert.json' ], controller, tryOpen );
-  } );
-
-  function tryOpen() {
-    try {
-      fs.statSync( resultPath );
-      controller.emit( "exits" );   
-    }
-    catch(err) {
-      controller.emit( "not exits" );
-    }
-  } 
-});
-
 test( 'data include', function(t) {
   
   var controller = new Expector(t);
